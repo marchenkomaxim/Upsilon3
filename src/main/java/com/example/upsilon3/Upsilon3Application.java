d4b806e6bd5b6ed8a73ddd49d862471be2d779ae
marchenkomaxim <songoci222@gmail.com>
2023-05-31 14:41:07 +0300

    Initial commit

---
+++ src/main/java/com/example/upsilon3/Upsilon3Application.java
@@ -0,0 +1,13 @@
+package com.example.upsilon3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Upsilon3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Upsilon3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/upsilon3/Upsilon3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.upsilon3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Upsilon3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
